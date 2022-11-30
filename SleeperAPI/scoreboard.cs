8283903233936bdf49efe350173dcee7872da09d
Samuel Gannon <samgannon@rocketmail.com>
2022-11-30 15:36:42 -0600

    // assign values

--- SleeperAPI/scoreboard.cs
+++ SleeperAPI/scoreboard.cs
@@ -10,10 +10,10 @@ namespace SleeperAPI
     internal class scoreboard
     {
         public int match_id { get; set; }
-        public string HomeTeam { get; set; }
-        public double HomeTeamPoints { get; set; }
-        public string AwayTeam { get; set; }
-        public double AwayTeamPoints { get; set; }
+        public int HomeTeam { get; set; }
+        public string HomeTeamPoints { get; set; }
+        public int AwayTeam { get; set; }
+        public string AwayTeamPoints { get; set; }
 
 
         public void ShowScoreboard(List<matchup> matchups, string weekNumber)
@@ -21,15 +21,19 @@ namespace SleeperAPI
             List<scoreboard> scoreboard = new();
             for (int i = 0; i < matchups.Count; i++)
             { 
+
                 for (int j = 0; j < matchups.Count; j++)
                 {
                     foreach (var matchup in matchups)
                     {
-                        if (matchups[i].matchup_id == matchups[j].matchup_id)
+                        if (matchups[i].matchup_id == matchups[j].matchup_id && matchups[i].roster_id != matchups[j].roster_id)
                         {
-                            // do something
                             
-
+                            match_id = matchups[i].matchup_id;
+                            HomeTeam = matchups[i].roster_id;
+                            HomeTeamPoints = matchups[i].points;
+                            AwayTeam = matchups[j].roster_id;
+                            AwayTeamPoints = matchups[j].points;
                         }
                     }
 
